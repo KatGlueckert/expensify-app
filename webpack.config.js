ed2551effa1061baf096b85c88419552e06d272a
Kat Glueckert <kaglueckert@crimson.ua.edu>
2020-07-07 13:37:42 -0500

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,54 @@
-
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env) => {
-    const isProduction = env === 'production';
-    const CSSExtract = new MiniCssExtractPlugin({filename: 'styles.css'});
-    
-    return {
-        entry: './public/src/app.js', 
-        output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'bundle.js'
-        },
-        module: {
-            rules: [{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    {loader: MiniCssExtractPlugin.loader},
-                    {loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                {loader: 'sass-loader',
-                    options: {
-                        sourceMap: true
-                    }
-                }
-                ],
-                
-            }],
-        },
-        plugins: [
-            CSSExtract
-        ],
-        devtool: isProduction ? 'source-map': 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
-        }
 
-    }
-}
+
+module.exports = (env) => {
+	const isProduction = env == 'production';
+	const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+	return {
+		entry:  './public/src/app.js',
+		output: {
+			path: path.join(__dirname, 'public', 'dist'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}]
+		},
+		plugins: [
+			CSSExtract,
+		
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+			publicPath: '/dist/'
+		}
+	};
+};
