5f9429f3370efdead4144631aa24b3d665a6d1a6
Kat Glueckert <kaglueckert@crimson.ua.edu>
2020-07-07 14:36:00 -0500

    Add expense summary to dashboard

--- public/src/components/expense-dashboard-page.js
+++ public/src/components/expense-dashboard-page.js
@@ -3,9 +3,11 @@ import ExpenseList from './expense-list';
 import ExpenseListFilters from './expense-list-filters';
 import {connect} from 'react-redux';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import ExpensesSummary from './expenses-summary';
 
  const ExpenseDashboardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
--- public/src/components/expense-list-item.js
+++ public/src/components/expense-list-item.js
@@ -11,7 +11,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt}) => (
     <h3 > {description}</h3>
     </Link>
     <p>
-    {numeral(amount).format('$0,0.00')} 
+    {numeral(amount/100).format('$0,0.00')} 
     -
      {moment(createdAt).format('MMMM, Do YYYY')}
     </p>
